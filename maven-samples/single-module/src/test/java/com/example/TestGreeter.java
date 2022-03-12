55dc0aa98d5f19ab5bdc1895f1cf48dbaafb2525
root <root@ip-172-31-4-207.ap-south-1.compute.internal>
2022-03-12 16:47:12 +0000

    Test fiel change

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -40,4 +40,12 @@ public class TestGreeter {
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
+
+  @Test
+  public void greetShouldIncludeGreetingWelcome() {
+    String someone = "World, Thanks you making me Happy";
+
+    assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
+  }
+
 }
